3c52549d5a395fd029907fb75bbac93f24eee267
Estela <emcalo1506@gmail.com>
2024-02-19 14:24:16 +0100

    UltimosArreglos

--- scripts.js
+++ scripts.js
@@ -198,3 +198,21 @@ document.addEventListener("DOMContentLoaded", () => {
     // Agregar un escuchador de eventos de cambio al campo de entrada de correo electrónico
     document.getElementById("f1-i5").addEventListener("change", handleEmailChange);
 });
+/* API */
+const dog_btn = document.getElementById('dog_btn');
+const dog_result = document.getElementById('dog_result');
+
+dog_btn.addEventListener('click', getRandomDog);
+
+function getRandomDog() {
+	fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+			}
+		});
+}
